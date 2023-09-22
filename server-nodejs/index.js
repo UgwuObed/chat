837338f18f69755b8a07d8e6d36e4589ec469d09
Ugwu Obed <vjazzyvictor@gmail.com>
2023-09-22 23:29:43 +0100

    fest

--- server-nodejs/index.js
+++ server-nodejs/index.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-const CHAT_ENGINE_PROJECT_ID = "";
-const CHAT_ENGINE_PRIVATE_KEY = "";
+const CHAT_ENGINE_PROJECT_ID = "b631f88f-be35-400c-9a51-3285640a6580";
+
 
 app.post("/signup", async (req, res) => {
   const { username, secret, email, first_name, last_name } = req.body;
@@ -18,7 +18,7 @@ app.post("/signup", async (req, res) => {
     const r = await axios.post(
       "https://api.chatengine.io/users/",
       { username, secret, email, first_name, last_name },
-      { headers: { "Private-Key": CHAT_ENGINE_PRIVATE_KEY } }
+      { headers: { "Private-Key": "5bd4e27a-1f29-48f3-9044-075e99ca13b9" } }
     );
     return res.status(r.status).json(r.data);
   } catch (e) {
@@ -29,8 +29,7 @@ app.post("/signup", async (req, res) => {
 app.post("/login", async (req, res) => {
   const { username, secret } = req.body;
 
-  // Fetch this user from Chat Engine in this project!
-  // Docs at rest.chatengine.io
+
   try {
     const r = await axios.get("https://api.chatengine.io/users/me/", {
       headers: {
@@ -45,5 +44,5 @@ app.post("/login", async (req, res) => {
   }
 });
 
-// vvv On port 3001!
+
 app.listen(3001);
