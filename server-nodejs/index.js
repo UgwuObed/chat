fdd815f1f8cd176232ca0ff0de701fc8446f3f3a
Ugwu Obed <vjazzyvictor@gmail.com>
2023-09-23 06:24:37 +0100

    fest

--- server-nodejs/index.js
+++ server-nodejs/index.js
@@ -6,19 +6,22 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-const CHAT_ENGINE_PROJECT_ID = "b631f88f-be35-400c-9a51-3285640a6580";
+const CHAT_ENGINE_PROJECT_ID = process.env.CHAT_ENGINE_PROJECT_ID;
+const CHAT_ENGINE_PRIVATE_KEY = process.env.CHAT_ENGINE_PRIVATE_KEY;
 
+if (!CHAT_ENGINE_PROJECT_ID || !CHAT_ENGINE_PRIVATE_KEY) {
+  console.error("Please set CHAT_ENGINE_PROJECT_ID and CHAT_ENGINE_PRIVATE_KEY environment variables.");
+  process.exit(1);
+}
 
 app.post("/signup", async (req, res) => {
   const { username, secret, email, first_name, last_name } = req.body;
 
-  // Store a user-copy on Chat Engine!
-  // Docs at rest.chatengine.io
   try {
     const r = await axios.post(
       "https://api.chatengine.io/users/",
       { username, secret, email, first_name, last_name },
-      { headers: { "Private-Key": "5bd4e27a-1f29-48f3-9044-075e99ca13b9" } }
+      { headers: { "Private-Key": CHAT_ENGINE_PRIVATE_KEY } }
     );
     return res.status(r.status).json(r.data);
   } catch (e) {
@@ -28,7 +31,6 @@ app.post("/signup", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   const { username, secret } = req.body;
-
 
   try {
     const r = await axios.get("https://api.chatengine.io/users/me/", {
@@ -44,5 +46,7 @@ app.post("/login", async (req, res) => {
   }
 });
 
-
-app.listen(3001);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
